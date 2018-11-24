839f24651895a22e835db38d157f3c44ac97a5fc
Abhinav Pillai <Abhinav.Pillai@omniware.in>
2018-11-24 21:19:32 +0530

    Added Changes.

--- app/src/main/java/com/test/pg/sampleapp/SampleAppConstants.java
+++ app/src/main/java/com/test/pg/sampleapp/SampleAppConstants.java
@@ -1,11 +1,11 @@
 package com.test.pg.sampleapp;
 
 public class SampleAppConstants {
-    //API_KEY is given by the Payment Gateway. Please Copy Paste Here.
+    //API_KEY is given by TraknPay. Please Copy Paste Here.
     public static final String PG_API_KEY = "<your_API_key>";
 
-    //HOSTNAME is given by the Payment Gateway. Please Copy Paste Here.
-    public static final String PG_HOSTNAME = "<traknpay_hostname>";
+    //HOSTNAME is given by TraknPay. Please Copy Paste Here.
+    public static final String PG_HOSTNAME = "https://biz.traknpay.in";
 
     //URL to Accept Payment Response After Payment. This needs to be done at the client's web server.
     public static final String PG_RETURN_URL = "<your_return_url>";
@@ -16,10 +16,10 @@ public class SampleAppConstants {
     //Enter the Mode of Payment Here . Allowed Values are "LIVE" or "TEST".
     public static final String PG_MODE = "TEST";
 
-    //PG_CURRENCY is given by the Payment Gateway. Only "INR" Allowed.
+    //PG_CURRENCY is given by TraknPay. Only "INR" Allowed.
     public static final String PG_CURRENCY = "INR";
 
-    //PG_COUNTRY is given by the Payment Gateway. Only "IND" Allowed.
+    //PG_COUNTRY is given by TraknPay. Only "IND" Allowed.
     public static final String PG_COUNTRY = "IND";
 
 
